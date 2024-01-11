42a9d767079a695ca0b124dcf2484fbf4ed60c73
Raditya Prima <139445331+superyorpa@users.noreply.github.com>
2024-01-11 17:12:34 +0700

    Add files via upload

---
+++ Phase 0/Part 7.5/soal1.js
@@ -0,0 +1,8 @@
+// Tugas 1
+// Buatlah sebuah fungsi bernama shoutOut(), yang mengembalikan nilai berupa "Halo Function!", 
+// yang kemudian akan ditampilkan di console.
+
+function shoutOut() {
+    return 'Hello Function!';
+}
+console.log(shoutOut());
---
+++ Phase 0/Part 7.5/soal3.js
@@ -0,0 +1,16 @@
+// Tugas 3
+// Buatlah sebuah fungsi bernama processSentence(), yang akan memproses seluruh parameter yang diinput menjadi 
+// satu kalimat berikut: 
+// "Nama saya [Name], umur saya [Age] tahun, alamat saya di [Address], dan saya punya hobby yaitu [hobby]!"
+
+function processSentence(name, age, address, hobby) {
+    return `Nama saya ${name}, umur saya ${age} tahun, alamat saya di ${address}, dan saya punya hobby yaitu ${hobby}`;
+}
+
+let name = "Agus";
+let age = 30;
+let address = "Jln. Malioboro, Yogjakarta";
+let hobby = "gaming";
+
+let fullSentence = processSentence(name, age, address, hobby);
+console.log(fullSentence); 

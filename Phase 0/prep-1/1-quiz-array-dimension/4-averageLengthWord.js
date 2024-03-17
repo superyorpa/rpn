d797495db123764bfebdf1736463063d5eebc755
Raditya Prima <139445331+superyorpa@users.noreply.github.com>
2024-03-17 16:41:53 +0700

    preparation-phase-1

---
+++ Phase 0/prep-1/1-quiz-array-dimension/4-averageLengthWord.js
@@ -0,0 +1,41 @@
+/*
+==================================
+Array Mastery: Average Length Word
+==================================
+Nama:________
+[INSTRUKSI]
+Disediakan sebuah kalimat. Function averageLengthWord akan menerima satu parameter berupa string
+yang berisikan kalimat tersebut, dan akan mendapatkan rata-rata jumlah huruf dari setiap kata,
+kemudian mengembalikan nilai berupa array of string yang berisikan kata mana saja yang jumlahnya
+sama dengan rata-rata jumlah kata.
+
+NOTE:
+ - Spasi tidak dihitung
+ - Jika hasil rata-rata adalah desimal maka dibulatkan
+
+[CONTOH]
+input (kalimat): Do you want to become a great coder.
+rata-rata panjang kata dalam kalimat: 3
+output: ['you']
+
+input (kalimat): You dont know what you have until you lose it!.
+rata-rata panjang kata dalam kalimat: 4
+output: ['dont', 'know', 'what', 'have', 'lose']
+*/
+
+function averageLengthWord(words) { 
+    let avg = Math.round(words.split(' ').join('').length / words.split(' ').length); 
+    let arrWord = words.split(' ');
+
+    let res = [];
+    for(let i = 0; i < arrWord.length; i++) {
+        if(arrWord[i].length === avg) {
+            res.push(arrWord[i])
+        }
+    }
+    return res;
+}
+  
+  console.log(averageLengthWord('Do you want to become a great coder ?')); // ['you']
+  console.log(averageLengthWord('You dont know what you have until you lose it!')); // [ 'dont','know','what','have','lose']
+  console.log(averageLengthWord('I am diligent')); // []
